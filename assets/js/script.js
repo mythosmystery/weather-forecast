ab2ad2173a23af9a82f2deb36b28134f7bc86047
Hunter Barton <axiysfire@gmail.com>
2021-03-27 22:21:20 -0500

    improved responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
-//api key 1b0d53466c6b55d0c85f1d56f3184390
+
 const units = "imperial";
 const key = "1b0d53466c6b55d0c85f1d56f3184390";
-//const defaultCity = "Oklahoma City";
+
+//url constants
 const iconURL = "http://openweathermap.org/img/wn/";
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 const onecallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 
+//weather button card vars
 var weatherBtnEl = $("#getWeather");
 var cityInputEl = $("#cityInput");
 var invalidCityEl = $("#invalidCity");
 
+//current weather card vars
 var currentWeatherCardEl = $("#currentWeather");
 var cityNameEl = $("#cityName");
 var currentTempEl = $("#currentTemp");
@@ -20,6 +23,7 @@ var currentIconEl = $("#currentIcon");
 
 var recentSearchesEl = $("#recentSearches");
 
+//forecast card arrays
 var forecastCardEl = $("#forecast");
 var forecastTempEl = [$("#day1temp"), $("#day2temp"), $("#day3temp"), $("#day4temp"), $("#day5temp")];
 var forecastIconEl = [$("#day1Icon"), $("#day2Icon"), $("#day3Icon"), $("#day4Icon"), $("#day5Icon")];
@@ -33,6 +37,7 @@ var forecastHumidityEl = [
 ];
 var forecastCityEl = $("#forecastCity");
 
+//empty data vars
 var currentTemp;
 var currentHumidity;
 var currentWindSpeed;
@@ -43,32 +48,40 @@ var cityName;
 
 var currentDate = moment().format("MM/DD/YYYY");
 
+//this gets saved to localstorage
 var cityList = [];
 
+//create a cities entry if there isnt one already
 if (localStorage.getItem("cities") == null) {
    localStorage.setItem("cities", JSON.stringify(cityList));
 }
+
 loadCityList();
 
+//hide unused cards
 invalidCityEl.hide();
 currentWeatherCardEl.hide();
 forecastCardEl.hide();
 
 recentSearchesEl.on("click", getRecentSearch);
 
+//sets cityname for future use and calls functions
 weatherBtnEl.on("click", function () {
    cityName = cityInputEl.val();
    getCurrentWeather(cityName);
    setRecentSearch();
 });
-function loadCityList(){
-  var cityArray = JSON.parse(localStorage.getItem("cities"));
-  console.log(cityArray);
-  for (let i = 0; i < cityArray.length; i++) {    
-    createButton(cityArray[i]);
-  }
+
+//loads city names from localstorage
+function loadCityList() {
+   var cityArray = JSON.parse(localStorage.getItem("cities"));
+   console.log(cityArray);
+   for (let i = 0; i < cityArray.length; i++) {
+      createButton(cityArray[i]);
+   }
 }
 
+//gets the current weather from the api
 function getCurrentWeather(cityID) {
    fetch(apiURL + cityID + "&appid=" + key + "&units=" + units)
       .then(function (resp) {
@@ -87,6 +100,8 @@ function getCurrentWeather(cityID) {
          invalidCityEl.show();
       });
 }
+
+//gets other data from open weather maps
 function getOneCall(lat, lon) {
    fetch(onecallURL + lat + "&lon=" + lon + "&exclude=hourly,minutely&units=" + units + "&appid=" + key)
       .then(function (resp) {
@@ -105,6 +120,8 @@ function getOneCall(lat, lon) {
          // catch any errors
       });
 }
+
+//sets the forecast cards from the arrays
 function setForecast() {
    forecastCardEl.show();
    forecastCityEl.text(cityName);
@@ -117,6 +134,8 @@ function setForecast() {
       forecastHumidityEl[i].text(forecast[i].humidity);
    }
 }
+
+//sets the current weather from the data retrived earlier
 function setCurrentWeather() {
    invalidCityEl.hide();
    currentWeatherCardEl.show();
@@ -128,6 +147,8 @@ function setCurrentWeather() {
    currentUVIndexEl.text(currentUVIndex);
    setUVColor();
 }
+
+//sets the color of the uv index item
 function setUVColor() {
    if (currentUVIndex < 3) {
       currentUVIndexEl.removeClass();
@@ -146,26 +167,33 @@ function setUVColor() {
       currentUVIndexEl.addClass("extreme");
    }
 }
+
+//gets data from recent search when clicked
 function getRecentSearch(event) {
    var currentButton = $(event.target);
    cityName = currentButton.text();
    console.log(cityName);
    getCurrentWeather(cityName);
 }
+
+//sets recent searches from buttons and adds to localstorage
 function setRecentSearch() {
    var buttonArray = recentSearchesEl.find("button");
-   var duplicate = false;
+   var invalid = false;
    cityList = JSON.parse(localStorage.getItem("cities"));
    for (let i = 0; i < buttonArray.length; i++) {
       console.log(buttonArray[i].textContent);
-      if (buttonArray[i].textContent == cityName) duplicate = true;
+      if (buttonArray[i].textContent == cityName) invalid = true;
+      if (cityName == "" || cityName == null) invalid = true;
    }
-   if (!duplicate) {
+   if (!invalid) {
       cityList.push(cityName);
       createButton(cityName);
-      localStorage.setItem("cities", JSON.stringify(cityList));      
+      localStorage.setItem("cities", JSON.stringify(cityList));
    }
 }
+
+//creates a button in the list with the correct bootstrap classes
 function createButton(name) {
    var newButton = $("<button type='button'>");
    newButton.addClass("list-group-item list-group-item-action");
